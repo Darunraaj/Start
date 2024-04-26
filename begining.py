81e19b15020d73d4b4107f74e383f326f8f4a5af
Darunraaj <darunraaj16@gmail.com>
2024-04-26 14:50:55 +0530

    commiting

---
+++ begining.py
@@ -0,0 +1,5 @@
+from file import find_max
+a = [2,6,9,4,55,58,2]
+
+x = find_max(a)
+print (x)
--- demo.py
+++ demo.py
@@ -1,1 +1,28 @@
 
+ask = ""
+started = False
+while True:
+    ask = input('> ').lower()
+    if ask == "quit":
+        break
+    elif ask == "start":
+        if started :
+            print('Car already started !')
+        else:
+            started = True
+            print ("Car started...Ready to go!")
+    elif ask == "stop":
+        if not started :
+            print('Car is already stopped!')
+        else:
+            started = False
+            print("Car stopped!")
+    elif ask == 'help':
+        print('''
+start - start the car
+stop - stop the car 
+quite - to exit car
+              ''')
+    else:
+        print("I don't understand that...")
+
