5f55a14a2e4bb4faf74b92b5f9ae5e7493eafcd7
Darunraaj <darunraaj16@gmail.com>
2024-04-28 16:25:49 +0530

    poblem

---
+++ two sum.py
@@ -0,0 +1,13 @@
+nums = [5,4,19,2,8,2]
+target = 10
+
+if nums[0]+nums[1]== target:
+    print ("output= [0,1]")
+elif nums[1]+nums[2]== target:
+    print ("output= [1,2]")
+elif nums[2]+nums[3]== target:
+    print ("output= [2,3]")
+elif nums[3]+nums[4]== target:
+    print ("output= [3,4]")
+elif nums[4]+nums[5]== target:
+    print ("output= [4,5]")
